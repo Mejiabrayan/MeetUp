cee05eab625ac2110d8315f5a6cee8bf3168b3db
mejiabrayan <learnwithbrayan@gmail.com>
2022-10-04 13:35:51 -0700

    Updates

---
+++ precache-manifest.60bc0021bf298a9f42cbef5a62c1085f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8ab6b5d820f0fd03096",
+    "url": "/MeetUp/static/css/main.ddbec41f.chunk.css"
+  },
+  {
+    "revision": "d8ab6b5d820f0fd03096",
+    "url": "/MeetUp/static/js/main.0cde5370.chunk.js"
+  },
+  {
+    "revision": "d3903d2d2c9243322f52",
+    "url": "/MeetUp/static/js/runtime~main.07743d1d.js"
+  },
+  {
+    "revision": "d9341d9d62a007e46cf9",
+    "url": "/MeetUp/static/js/2.d4b86167.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/MeetUp/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "93ebaa6850c343cc91ea48fbf2e3977f",
+    "url": "/MeetUp/index.html"
+  }
+];
